7cee6562bf7c1a261095a61b36963abb2878fd33
Joyce <qyliu10@gmail.com>
2022-09-01 09:57:39 -0700

    add validation on signup

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { Redirect } from 'react-router-dom';
 import { signUp } from '../../store/session';
+import './SignUpForm.css';
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
@@ -12,9 +13,35 @@ const SignUpForm = () => {
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
 
+
+
+
+
+  useEffect(() => {
+    let errors = [];
+    if (username.length < 3) {
+      errors.push('Username must be longer than 3 characters.')
+    }
+    if (!(/[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]{2,3}/.test(email))) {
+      errors.push('Email must be in the format like example@gmail.com.')
+    }
+    if (password.length < 4) {
+      errors.push('Password must be at least 4 charaters or more.')
+    }
+    if (password !== repeatPassword) {
+      errors.push('Repeat password must be the same as the password field.')
+    }
+    setErrors(errors);
+
+  }, [username, email, password, repeatPassword])
+
+
+
+
+
   const onSignUp = async (e) => {
     e.preventDefault();
-    if (password === repeatPassword) {
+    if (password === repeatPassword && !errors.length) {
       const data = await dispatch(signUp(username, email, password));
       if (data) {
         setErrors(data)
@@ -39,55 +66,63 @@ const SignUpForm = () => {
   };
 
   if (user) {
-    return <Redirect to='/' />;
+    return <Redirect to='/explore' />;
   }
 
   return (
-    <form onSubmit={onSignUp}>
+    <div>
       <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+
       </div>
       <div>
-        <label>User Name</label>
-        <input
-          type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
-        ></input>
+        <form onSubmit={onSignUp}>
+          <div className='signupErr'>
+            {errors.map((error, ind) => (
+              <div key={ind}>* {error}</div>
+            ))}
+          </div>
+          <div>
+            <label>User Name</label>
+            <input
+              type='text'
+              name='username'
+              onChange={updateUsername}
+              value={username}
+            ></input>
+          </div>
+          <div>
+            <label>Email</label>
+            <input
+              type='text'
+              name='email'
+              onChange={updateEmail}
+              value={email}
+            ></input>
+          </div>
+          <div>
+            <label>Password</label>
+            <input
+              type='password'
+              name='password'
+              onChange={updatePassword}
+              value={password}
+            ></input>
+          </div>
+          <div>
+            <label>Repeat Password</label>
+            <input
+              type='password'
+              name='repeat_password'
+              onChange={updateRepeatPassword}
+              value={repeatPassword}
+              required={true}
+            ></input>
+          </div>
+          <button type='submit'>Sign Up</button>
+        </form>
       </div>
-      <div>
-        <label>Email</label>
-        <input
-          type='text'
-          name='email'
-          onChange={updateEmail}
-          value={email}
-        ></input>
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type='password'
-          name='password'
-          onChange={updatePassword}
-          value={password}
-        ></input>
-      </div>
-      <div>
-        <label>Repeat Password</label>
-        <input
-          type='password'
-          name='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-        ></input>
-      </div>
-      <button type='submit'>Sign Up</button>
-    </form>
+    </div>
+
   );
 };
 
