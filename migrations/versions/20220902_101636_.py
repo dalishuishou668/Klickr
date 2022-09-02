37c4ecbab7d3b739bd0e584d253416c8abe8efca
Joyce <qyliu10@gmail.com>
2022-09-02 10:18:19 -0700

    change user table

--- migrations/versions/20220902_101636_.py
+++ migrations/versions/20220902_101636_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: ce4f22d323b7
+Revision ID: 7245d0991e36
 Revises: 
-Create Date: 2022-08-31 14:53:37.495314
+Create Date: 2022-09-02 10:16:36.210401
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'ce4f22d323b7'
+revision = '7245d0991e36'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -23,6 +23,7 @@ def upgrade():
     sa.Column('username', sa.String(length=40), nullable=False),
     sa.Column('email', sa.String(length=255), nullable=False),
     sa.Column('hashed_password', sa.String(length=255), nullable=False),
+    sa.Column('profile_pic', sa.Text(), nullable=True),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('email'),
     sa.UniqueConstraint('username')
